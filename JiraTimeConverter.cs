51e14f497eb0ff7bb0478953e0cd1ce5c7af6829
DaniilPalii <palii.daniil.o@gmail.com>
2020-04-23 16:19:34 +0200

    Refactor

---
+++ JiraTimeConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace JiraTimeConverter
+{
+    public class JiraTimeConverter
+    {
+        public double ConvertToNormalHours(string[] jiraTimeItems)
+        {
+            var totalHours = 0d;
+
+            foreach (var item in jiraTimeItems)
+            {
+                if (weekRegex.TryMatchInt(item, out var weeks))
+                {
+                    totalHours += weeks.Value * DaysInWeek * HoursInDay;
+                }
+                else if (dayRegex.TryMatchInt(item, out var days))
+                {
+                    totalHours += days.Value * HoursInDay;
+                }
+                else if (hourRegex.TryMatchInt(item, out var hours))
+                {
+                    totalHours += hours.Value;
+                }
+                else if (minutesRegex.TryMatchInt(item, out var minutes))
+                {
+                    totalHours += (double) minutes.Value / MinutesInHour;
+                }
+                else
+                {
+                    Console.WriteLine($"Error: can't parse \"{item}\"");
+                }
+            }
+
+            return totalHours;
+        }
+        
+        private static Regex CreateJiraTimeItemRegex(char smallItemLetter, char bigItemLetter)
+        {
+            return new Regex($@"(?<=(\s|^))\d+(?=({smallItemLetter}|{bigItemLetter})(\s|$))");
+        }
+
+        private readonly Regex weekRegex = CreateJiraTimeItemRegex(smallItemLetter: 'w', bigItemLetter: 'W');
+        private readonly Regex dayRegex = CreateJiraTimeItemRegex(smallItemLetter: 'd', bigItemLetter: 'D');
+        private readonly Regex hourRegex = CreateJiraTimeItemRegex(smallItemLetter: 'h', bigItemLetter: 'H');
+        private readonly Regex minutesRegex = CreateJiraTimeItemRegex(smallItemLetter: 'm', bigItemLetter: 'M');
+
+        private const int DaysInWeek = 5;
+        private const int HoursInDay = 8;
+        private const int MinutesInHour = 60;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text.RegularExpressions;
 
 namespace JiraTimeConverter
 {
@@ -7,65 +6,27 @@ namespace JiraTimeConverter
     {
         public static void Main(string[] args)
         {
-            var isItInteractiveMode = false;
-
-            if (args.Length == 0)
+            if (args.Length > 0)
             {
-                isItInteractiveMode = true;
+                WriteTotalNormalHours(args);
 
-                Console.Write("Please, input a JIRA time: ");
-                args = Console.ReadLine().Split(' ');
+                return;
             }
 
-            var totalHours = CountTotalNormalHours(args);
-            Console.WriteLine($"Total normal hours: {totalHours}");
+            Console.Write("Please, input a JIRA time: ");
+            var jiraTimeItems = Console.ReadLine().Split(' ');
 
-            if (isItInteractiveMode)
-            {
-                Console.WriteLine();
-                Console.WriteLine("Press any key to exit...");
-                Console.ReadKey();
-            }
+            WriteTotalNormalHours(jiraTimeItems);
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
 
-        private static double CountTotalNormalHours(string[] args)
+        private static void WriteTotalNormalHours(string[] jiraTimeItems)
         {
-            var totalHours = 0d;
-
-            foreach (var item in args)
-            {
-                if (WeekRegex.TryMatchInt(item, out var weeks))
-                {
-                    totalHours += weeks.Value * DaysInWeek * HoursInDay;
-                }
-                else if (DayRegex.TryMatchInt(item, out var days))
-                {
-                    totalHours += days.Value * HoursInDay;
-                }
-                else if (HourRegex.TryMatchInt(item, out var hours))
-                {
-                    totalHours += hours.Value;
-                }
-                else if (MinutesRegex.TryMatchInt(item, out var minutes))
-                {
-                    totalHours += (double)minutes.Value / MinutesInHour;
-                }
-                else
-                {
-                    Console.WriteLine($"Error: can't understand \"{item}\"");
-                }
-            }
-
-            return totalHours;
+            var totalHours = new JiraTimeConverter().ConvertToNormalHours(jiraTimeItems);
+            Console.WriteLine($"Total normal hours: {totalHours}");
         }
-
-        private static readonly Regex WeekRegex = new Regex(@"(?<=(\s|^))\d+(?=(w|W)(\s|$))");
-        private static readonly Regex DayRegex = new Regex(@"(?<=(\s|^))\d+(?=(d|D)(\s|$))");
-        private static readonly Regex HourRegex = new Regex(@"(?<=(\s|^))\d+(?=(h|H)(\s|$))");
-        private static readonly Regex MinutesRegex = new Regex(@"(?<=(\s|^))\d+(?=(m|M)(\s|$))");
-
-        private const int DaysInWeek = 5;
-        private const int HoursInDay = 8;
-        private const int MinutesInHour = 60;
     }
 }
